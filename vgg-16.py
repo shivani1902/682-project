51337e8632fb8aaa41ac3e31c5bd17f4248b70db
shivani1902 <shivanigupta@cs.umass.edu>
2017-12-10 13:51:20 -0500

    Add files via upload

---
+++ vgg-16.py
@@ -0,0 +1,129 @@
+from keras.models import Sequential,Model
+from keras.layers.core import Flatten, Dense, Dropout
+from keras import optimizers
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD,rmsprop
+import numpy as np
+from collections import defaultdict
+import operator
+from skimage import data, io, filters, transform
+import skimage
+from itertools import islice
+from keras import applications
+from pathlib2 import Path
+import os.path
+
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(islice(iterable, n))
+
+def read_image_tags():
+    alltags = set()
+    image_data = {}
+    tags_file = open("../HARRISON/tag_list.txt")
+    tags = tags_file.readlines()
+   # print tags
+    image_file = open("../HARRISON/data_list.txt")
+    lines = image_file.readlines()
+    count = 0
+    for line in lines:
+        path = os.path.join("../HARRISON/"+line.strip())
+        if os.path.exists(path):
+            image_data[line] = tags[count].split()
+            for i in tags[count].split():
+                alltags.add(i)
+        count+=1
+       # print image_data, alltags
+    return image_data, list(alltags)
+
+def encode_tags(images, n,tags):
+	#print images
+	encoded_tags = []
+	for value in images.values():
+		#print value
+		temp = np.zeros(n)
+    	for i in value:
+        	temp[tags.index(i)] = 1
+    	encoded_tags.append(temp)
+	return encoded_tags
+
+def load_image(path):
+    # load image
+    img = skimage.io.imread(path)
+    img = img / 255.0
+    assert (0 <= img).all() and (img <= 1.0).all()
+    # print "Original Image Shape: ", img.shape
+    # we crop image from center
+    short_edge = min(img.shape[:2])
+    yy = int((img.shape[0] - short_edge) / 2)
+    xx = int((img.shape[1] - short_edge) / 2)
+    crop_img = img[yy: yy + short_edge, xx: xx + short_edge]
+    # resize to 224, 224
+    resized_img = skimage.transform.resize(crop_img, (224, 224,3))
+    return resized_img
+
+def read_image_data(images):
+	#read images
+	image_data = []
+	count=0
+	#print images
+	for key in images.keys():
+		#if count%10000==0:
+			#print count
+		#print key
+		count = count + 1
+		#print key       
+		im = load_image("../HARRISON/"+key.strip())
+    	im -= np.mean(im)
+    	image_data.append(im)
+	return image_data
+
+def process_data():
+	images,tags = read_image_tags()
+#	print images
+	encoded_tags = encode_tags(images, len(tags),tags)
+	print "done encoding"
+	image_data = read_image_data(images)
+	print "reading"
+	image_data = np.array(image_data).astype(np.float32)
+	print len(image_data)
+	return image_data, encoded_tags, tags, images
+
+
+def model(n):
+	img_rows, img_cols, img_channel = 224, 224, 3
+
+	base_model = applications.VGG16(weights='imagenet', include_top=False, input_shape=(img_rows, img_cols, img_channel))
+	add_model = Sequential()
+	add_model.add(Flatten(input_shape=base_model.output_shape[1:]))
+	add_model.add(Dense(n, activation='sigmoid'))
+
+	model = Model(inputs=base_model.input, outputs=add_model(base_model.output))
+	model.compile(loptimizer='rmsprop', loss='binary_crossentropy',metrics=['accuracy'])
+	return model
+
+def train():
+	image_data, encoded_tags, tags, image_with_tags = process_data()
+	#X_train = image_data[:45906]
+	#Y_train = encoded_tags[:45906]
+	#X_test = image_data[45906:]
+	#Y_test = image_data[45906:]
+	X_train = image_data[:50]
+	Y_train = image_data[:50]
+
+	History = model.fit(np.array(X_train),np.array(Y_train), epochs=5, batch_size=256,validation_split=0.2,shuffle=True )
+	with open('/trainHistoryDict', 'wb') as file_pi:
+		pickle.dump(history.history, file_pi)
+    # serialize model to JSON
+	model_json = model.to_json()
+	with open("model.json", "w") as json_file:
+		json_file.write(model_json)
+# serialize weights to HDF5
+	model.save_weights("model.h5")
+	print("Saved model to disk")
+
+if __name__ == '__main__':
+    # start_time = datetime.now()
+    train()
+
+
